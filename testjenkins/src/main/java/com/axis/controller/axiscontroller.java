4ec6f2da805344fdd782261574daede461b8be7c
99003577 <priyansh.pandey@ltts.com>
2021-07-23 11:20:38 +0530

    first commit

---
+++ testjenkins/src/main/java/com/axis/controller/axiscontroller.java
@@ -0,0 +1,21 @@
+package com.axis.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/axis")
+public class axiscontroller {
+
+	@GetMapping("/test")
+	public String axistest() {
+		return "welcome to axis bank";
+	}
+	
+	
+}
+
+
+
